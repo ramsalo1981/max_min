db51314ac34031261c3d835114e7b8c129ce523b
rsalo1981 <55482188+rsalo1981@users.noreply.github.com>
2019-09-29 17:02:16 +0200

    Add project files.

---
+++ max_min/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace max_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to know max and min value of char and int ...etc
+            int charMin = char.MinValue;
+            int charMax = char.MaxValue;
+
+            int mycharvalue = 'g';
+
+            Console.WriteLine($"min char = {charMin} max char = {charMax} my char value= {mycharvalue}");
+
+            int intmin = int.MinValue;
+            int intmax = int.MaxValue;
+
+            Console.WriteLine($"min value = {intmin} max value = {intmax}");
+
+        }
+    }
+}
